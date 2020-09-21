79844f81be219e360154168766d3273240cea965
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-21 08:25:01 -0700

    Remove Dev Post Link

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,12 +1,9 @@
 import styles from '../styles/BlogPost.module.css'
 
-import { Link } from '@material-ui/core'
-
 export default function BlogPost({ postLink, postTitle }) {
     return (
         <footer className={styles.blogpost}>
-            <h4>{postTitle}</h4>
-            <Link href={postLink} color="primary">DEV Post</Link>
+            <h4><a href={postLink} target="_blank">{postTitle}</a></h4>
         </footer>
     )
 }
