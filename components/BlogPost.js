670369619dab51f2834afc37b3dea450e06585f1
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-21 11:56:10 -0700

    Tag Links

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -4,7 +4,7 @@ export default function BlogPost({ postLink, postTitle, tags }) {
     return (
         <footer className={styles.blogpost}>
             <h4><a href={postLink} target="_blank">{postTitle}</a></h4>
-            <div>{tags}</div>
+            <div>{tags.map((tag, index) => <a target="_blank" key={index} href={`https://dev.to/t/${tag}`}>{tag} </a>)}</div>
         </footer>
     )
 }
