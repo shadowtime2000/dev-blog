b3a18dd4302e7b4a3aeeaf3ebe51177f2fadff29
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-13 09:51:53 -0700

    fix(api): attempted fix on api

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  axios
-    .get("https://dev.to/api/articles/me/published", {
-      headers: { "api-key": process.env.API_KEY },
-    })
-    .then((resp) => resp.data)
-    .then((resp) => res.end(JSON.stringify(resp)));
+  const { data } = await axios.get("https://dev.to/api/articles/me/published", {
+    headers: { "api-key": process.env.API_KEY },
+  });
+  res.end(JSON.stringify(data));
 }
