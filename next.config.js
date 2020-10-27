69b542de193b3030630336bceea94ea540574647
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-27 15:16:46 -0700

    feat: turn off no indexing header

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: "/",
+                headers: [
+                    {
+                        key: "X-Robots-Tag",
+                        value: ""
+                    }
+                ]
+            }
+        ]
+    }
+}
