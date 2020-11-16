f717af26f6cb3fbd9173e97e37590421c6172915
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-16 12:59:40 -0800

    refactor: dont use material ui

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,13 @@
 import { memo } from "react";
-import { Link } from "@material-ui/core";
 import styles from "../styles/Footer.module.css";
 
 function Footer() {
   return (
     <div className={styles.footer}>
       Created with ❤️ by{" "}
-      <Link href="https://github.com/shadowtime2000" target="_blank">
+      <a href="https://github.com/shadowtime2000" target="_blank">
         shadowtime2000
-      </Link>
+      </a>
     </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -2,18 +2,16 @@ import { memo } from "react";
 
 import styles from "../styles/Header.module.css";
 
-import { Link } from "@material-ui/core";
-
 function Header() {
   return (
     <>
       <h1 className={styles.title}>
-        <Link
+        <a
           href={`https://dev.to/${process.env.NEXT_PUBLIC_USERNAME}`}
           target="_blank"
         >
           {process.env.NEXT_PUBLIC_USERNAME}
-        </Link>
+        </a>
         's DEV Blog
       </h1>
     </>
