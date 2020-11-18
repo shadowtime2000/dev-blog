99c98ca895c9d73a71c70ea59a50929e5005902d
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-18 15:59:50 -0800

    some changes

--- pages/[id].js
+++ pages/[id].js
@@ -10,7 +10,13 @@ import Header from "../components/Header";
 import Profile from "../components/Profile";
 import Footer from "../components/Footer";
 
-function Post({ postContent, title, url, summary, profile_image }) {
+function Post({
+  postContent,
+  title,
+  url,
+  summary,
+  profile_image,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -48,7 +54,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(ctx) {
   const {
-    data: { body_markdown, title, url },
+    data: { body_markdown, title, url, },
   } = await axios.get(`https://dev.to/api/articles/${ctx.params.id}`);
   const {
     data: { summary, profile_image },
