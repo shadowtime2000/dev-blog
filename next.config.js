e4f139ee9cec8a03cde57cf1516e58dd1d9b03c6
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-23 19:57:33 -0800

    enable indexing for posts

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ module.exports = {
           },
         ],
       },
+      {
+        source: "/post/:id",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: ""
+          }
+        ]
+      }
     ];
   },
 };
