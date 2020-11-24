dfa7f83b451c6f04be999c4a6d8c36ed891cdb4f
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-23 19:50:27 -0800

    fix(config): turn off no indexing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/:path",
         headers: [
           {
             key: "X-Robots-Tag",
